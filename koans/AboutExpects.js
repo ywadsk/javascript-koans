94b56c99a4679ab0ec8b58fdca7f19ea5f3de9fb
Yong Wang <wangy@ads.autodesk.com>
2016-03-18 12:22:37 -0700

    change false to true

--- koans/AboutExpects.js
+++ koans/AboutExpects.js
@@ -4,6 +4,7 @@ describe('About Expects', function() {
   it('should expect true', function() {
 
     // Your journey begins here: Replace the word false with true 
+    expect(true).toBeTruthy();
     expect(false).toBeTruthy();
   });
 
